90bdee4ea78f56c9bcad529b5138acb636a56603
Ahmadin Anton <demonashik@mail.ru>
2025-03-10 20:37:03 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,66 @@
+// alert("Hello world")
+// console.log("Hello world")
+
+// let myFirstVariable = 50
+// let age = 60
+// console.log(myFirstVariable)
+// console.log(age)
+// // Тип данных number хранит в себе численные значения ( целые и числа с плавающей точкой)
+
+// let name1 = `Tony`
+// alert(name)
+// alert(`Мне ${age} лет`)
+
+// то что выше строка в комбинировании обязательно именно обратные скобки на кнопку "ё"
+
+// let boolValue = true
+// console.log(boolValue)
+
+// console.log(typeof(boolValue))
+
+// let result = prompt("Как дела?") // дает возможность что-то писать в строке всплывающего окна
+// let result = confirm("Как дела?") // не дает вохможность что-то писать внутри окна только ок или отмена 
+
+// let= задать значение чему-то кому-то дальше идет просто название любое 
+
+// let result = prompt("Как вас зовут?")
+// alert(result)
+
+// let name = prompt("Как тебя зовут?")
+// alert(`привет ${name}`)
+
+//Преобразование типов данных 
+
+// let value = true
+// console.log(typeof(value))
+
+// let transformation = String(value)
+// console.log(typeof(transformation)) 
+
+// let value1 = "50"
+// console.log(typeof(value1))
+
+// let transformation1 = Number(value1)
+// console.log(typeof(transformation1))
+
+// Базовые операторы , Математика Унарный (-), бинарный (+), операнд(Сами символы)
+// *-умножить (-)-минус (+)-плюс **-степень /-деление 
+
+// let value = 64
+// let value1= 2 
+// let result = value ** value1
+// console.log(result)
+
+//Конкатенация-соединение строк =одно яблокоодно яблоко
+// let str = "одно яблоко" + "одно яблоко"
+// console.log(str)
+
+//Инкриментор
+let counter = 2;
+counter++
+
+
+
+
+
+
