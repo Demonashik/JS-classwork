c5a05368cda528bdff97c98e5e0ccca4a9abf378
Ahmadin Anton <demonashik@mail.ru>
2025-03-19 20:39:52 +0300

    novaya para

---
+++ JS 19.03/js123.js
@@ -0,0 +1,109 @@
+
+// Function Declaration 
+
+/* function hello() {
+  alert('Привет')
+}
+hello()
+
+
+
+// Function Expression 
+let world = function() {
+    alert('Привет!')
+}
+world()
+ */
+
+
+/* let world = function() {
+  alert('Академия ТОП')
+}
+ let func = world;
+
+ func() 
+ */
+
+//CallBack function
+
+/*  function ask(question, yes, no) {
+  if (confirm(question)) yes()
+    else no()
+ }
+
+ function showOk() {
+  alert('Вы согласны')
+ }
+
+ function showcancel() {
+  alert('Отмена')
+ }
+
+ask('Вы согласны?', showOk, showcancel)  */
+
+/* let age = prompt("Сколько вам лет?")
+
+if (age < 18) {
+  welcome = function() {
+    alert('Привет!')
+  }
+
+} else {
+  welcome = function() {
+    alert("Здравствуйте")
+  }
+}
+welcome() */
+
+
+
+// Создайте функцию sayHello, которая выводит в консоль сообщение "Привет!", используя function expression.
+/* sayHello = function() {
+  console.log("Привет!")
+}
+sayHello()
+ */ //++
+
+// Создайте функцию doSomething, которая принимает callback-функцию и вызывает её. В качестве callback-функции передайте простую функцию, которая выводит в консоль "Я callback!".
+
+/*  let doSomething = function (callback) {
+  callback()
+}
+
+let mycallback = function(){
+  console.log('Я callback!')
+}
+doSomething(mycallback) */ //++
+
+
+
+// Создайте функцию multiply, которая принимает два числа и возвращает их произведение, используя function expression.
+
+/* let multiply = function(c,b) {
+  return c * b
+}
+console.log(multiply(2, 4 )) */ //++
+
+// Создайте функцию calculate, которая принимает два числа и callback-функцию. Callback-функция должна выполнить какую-то операцию с этими числами (например, сложение) и вернуть результат.
+/* let calculator = function(a,b,callback) {
+  return callback(a,b)
+}
+
+let add = function(x,y){
+  return x + y 
+}
+console.log(calculator(5,3, add)) */ //++
+
+// Простой callback
+// Создайте функцию askQuestion, которая принимает callback-функцию и вызывает её, передавая строку "Как дела?". Callback-функция должна вывести эту строку в консоль.
+
+/* let askQuestion = function(callback) {
+    callback('Как дела?')
+}
+let showQuestion = function(question) {
+  console.log(question)
+}
+askQuestion(showQuestion) */ // ++
+
+
+//
